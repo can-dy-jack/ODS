ae4fa56bd5ff7fea420f2b8ee244fa844553a6ff
kartjim <jjack_chen3@163.com>
2021-01-25 12:41:30 +0800

    1.0.03

--- release/miniprogram/pages/logs/logs.js
+++ release/miniprogram/pages/logs/logs.js
@@ -3,19 +3,25 @@ Page({
     list:[
       {
         id:0,
-        description: "start",
-        time: "2021-1-21 21:00:00",
+        description: "start", 
+        time: "2021-1-21 21:00:00"
       },
       {
         id:1,
-        description: "初步tabBar",
-        time: "2021-1-22 9:45:37",
+        description: "加上tabBar",
+        time: "2021-1-22 9:45:37"
       },
       {
         id:2,
-        description: "日志格式-wx:for",
-        time: "2021-1-24 15:47:55",
+        description: "日志格式-wx:for+wx:if",
+        time: "2021-1-24 15:47:55"
+      },
+      {
+        id:3,
+        description: "添加测试页面",
+        time: "2021-1-25 12:19:27"
       }
-    ]
+    ],
+    hasColor: true
   }
 })
--- release/miniprogram/pages/test/test.js
+++ release/miniprogram/pages/test/test.js
@@ -1,9 +1,30 @@
-// pages/home/home.js
 Page({
   data: {
     msg: "日志",
-    test: "测试"
+    date: 2021,
+    isFree: true,
+    num: 0,
+    datas: {
+      sum: 200,
+      tips: "海洋数据",
+      isFree: true
+    }
   },
+  // bindinput事件绑定
+  handleInput(e) { //e是事件源对象
+    this.setData({
+      num: e.detail.value
+    })
+  },
+  // bindtap事件绑定
+  handletap(e) {
+    // console.log(e);
+    const ope = e.currentTarget.dataset.operation;
+    this.setData({
+      num: this.data.num + ope
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -42,15 +63,21 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {},
+  onPullDownRefresh: function () {
+
+  },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {},
+  onReachBottom: function () {
+
+  },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {}
+  onShareAppMessage: function () {
+
+  }
 })
